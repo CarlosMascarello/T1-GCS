ec28bf5da77bf33f40c60befa037b9971543bc3c
Jayme Fortes <Jayme.fortes@edu.pucrs.br>
2024-10-05 14:26:08 -0300

    Classe cadastro item

---
+++ CadastroItem.java
@@ -0,0 +1,46 @@
+
+import java.util.ArrayList;
+
+/**
+ * Classe CadastroItem representa o cadastro de um novo item ao jogador
+ */
+public class CadastroItem {
+    private ArrayList<Item> itens;
+    private CadastroJogador cadastro;
+    private ArrayList<Jogador> jogadores;
+
+    public CadastroItem() {
+        this.itens = new ArrayList<>();
+        this.cadastro = new CadastroJogador();
+    }
+
+
+    public void addItemAoJogador(Item item, String pin) {
+        for (Jogador j : jogadores) {
+            if (j.getPin().equals(pin)) {
+                j.adicionarItem(item);
+                this.itens.add(item);
+                return;
+            }
+        }
+    }
+
+    public void removeItem(Item item, String pin) {
+        for (Jogador j : jogadores) {
+            if (j.getPin().equals(pin)) {
+                j.removerItem(item);
+                this.itens.remove(item);
+                return;
+            }
+        }
+    }
+
+    public Item buscarItemPorNome(String nomeItem) {
+        for (Item item : itens) {
+            if (item.getNome().equals(nomeItem)) {
+                return item;
+            }
+        }
+        return null;
+    }
+}
